0bbbdfb6807d314faa17d122e8bdb22432519d30
Aditya Chandel <adchandel@bebotechnologies.com>
2025-12-18 14:41:31 +0530

    first commit

--- tests/Check2.spec.js
+++ tests/Check2.spec.js
@@ -25,3 +25,19 @@ test('Upload', async ({ page }) => {
 
     await expect(page).toHaveURL('https://opensource-demo.orangehrmlive.com/web/index.php/pim/viewPhotograph/empNumber/7')
 });
+
+
+
+test('Upload 2', async ({ page }) => {
+  await page.getByText('My Info').click()
+
+  await page.locator('[class="employee-image"]').click()
+
+  await page.locator('[class="oxd-icon bi-plus"]').click()
+
+  // await page.setInputFiles('C:\Users\adchandel\Downloads\QASource Virtual Backgrounds')
+
+  // await page.setInputFiles('input.oxd-icon bi-plus','C:\Users\adchandel\Downloads\QASource Virtual Backgrounds\Virtual-Background-5.jpg')
+
+  await expect(page).toHaveURL('https://opensource-demo.orangehrmlive.com/web/index.php/pim/viewPhotograph/empNumber/7')
+});
